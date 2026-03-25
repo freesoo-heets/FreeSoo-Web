63406584497f6fd1c739cfecb4d9376c16443bd5
freesoo-heets <mirckzk32@gmail.com>
2026-03-26 02:33:45 +0900

    Add files via upload
    fist upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'freesoo-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './notices.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
